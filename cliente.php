b19534e5cd1ef873cd72dbfc311defeaa0980641
jonathan <jonathan@jonathan-ThinkCentre-M72e>
2017-01-11 15:50:28 -0500

    Carga masiva pago y pagodetalle

--- cliente.php
+++ cliente.php
@@ -1,28 +1,99 @@
 <?php
     require_once "nusoap/lib/nusoap.php";
     $cliente = new nusoap_client("http://localhost/servicio/servidor.php");
-      
-    $error = $cliente->getError();
+
     if ($error) {
         echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
     }
-      
-    $result = $cliente->call("getInsertarEmpleado", 
-            array("codigoTipoIdentificacion" => "CC",
-                "identificacion" => "70143086"));
-      
+    
+    
+    //datos empleado__________________________________
+    $tipoRegistro = 1;
+    $codigoTipoIdentificacion = 1;
+    $identificacion = 70143086;
+    $nombre1 = "jonathan";
+    $nombre2 = "alexander";
+    $apellido1 = "moncada";
+    $apellido2 = "duque";
+    $nombrecorto = $nombre1 . $nombre2 . $apellido1 . $apellido2;
+    
+    $paramsEmpleado = array(
+    'tipoRegistro' => $tipoRegistro,
+    'codigoTipoIdentificacion' => $codigoTipoIdentificacion,
+    'identificacion' => $identificacion,
+    'nombre1' => $nombre1,
+    'nombre2' => $nombre2,
+    'apellido1' => $apellido1,
+    'apellido2' => $apellido2,
+    'nombrecorto' => $nombrecorto);
+
+    //datos pago________________________________________
+   /* $tipoRegistroPago = 02;
+    $tipoPago = 1;
+    $fechaDesde = 2016-01-04;
+    $fechaHasta = 2016-01-08;
+    $numero = 23456789;
+    $vrDeduccion = 98;
+    $vrNeto = 87;
+    $vrDevengado = 76;
+    $tipodocumento = 1;
+    $numeroDocumento = 1234567;
+    
+    $paramsPago = array(
+    'tipoRegistroPago' => $tipoRegistroPago,
+    'tipoPago' => $tipoPago,
+    'fechaDesde' => date_create($fechaDesde),
+    'fechaHasta' => date_create($fechaHasta),
+    'numero' => $numero,
+    'vrDeduccion' => $vrDeduccion,
+    'vrNeto' => $vrNeto,
+    'vrDevengado' => $vrDevengado,
+    'tipodocumento' => $tipodocumento,
+    'numeroDocumento' => $numeroDocumento);*/
+
+    //datos pagoDetalle_____________________________________
+    /*$tipoRegistroPagoDetalle = 03;
+    $concepto = " concepto de pago por prestacion de servicios";
+    $vrPago = 12;
+    $operacion = 10;
+    $horas = 5;
+    $porcentaje = 8;
+    $dias = 7;
+    $vrNetoDetalle = 78987;
+    $numeroDetalleFk = 88888855;
+    
+    $paramsPagoDetalle = array(
+    'tipoRegistroPagoDetalle' => $tipoRegistroPagoDetalle,
+    'concepto' => $concepto,
+    'vrPago' => $vrPago,
+    'operacion' => $operacion,
+    'horas' => $horas,
+    'porcentaje' => $porcentaje,
+    'dias' => $dias,
+    'vrNetoDetalle' => $vrNetoDetalle,
+    'numeroDetalleFk' => $numeroDetalleFk);*/
+     
+    $result = $cliente->call("getInsertarEmpleado", $paramsEmpleado );
+    
+    //$result = $cliente->call("getInsertarPago", $paramsPago);
+
+   // $result = $cliente->call("getInsertarPagoDetalle", $paramsPagoDetalle);
+
     if ($cliente->fault) {
         echo "<h2>Fault</h2><pre>";
-        print_r($result);
+           print_r($respuesta);
         echo "</pre>";
     }
     else {
-        $error = $cliente->getError();
+    print_r($respuesta);
+        $error = $cliente->getError(); 
+       $client->request;
+        var_dump($cliente); // prints "test"
         if ($error) {
             echo "<h2>Error</h2><pre>" . $error . "</pre>";
         }
         else {
-            echo "<h2>Libros</h2><pre>";
+            echo "<h2>Cargar Datos</h2><pre>";
             echo $result;
             echo "</pre>";
         }
--- servidor.php
+++ servidor.php
@@ -1,41 +1,108 @@
 <?php
-    require_once "nusoap/lib/nusoap.php";
-      
-    function getInsertarEmpleado($codigoTipoIdentificacion, $identificacion) {
-        return "Se inserto el empleado";
-        /*if ($categoria == "libros") {
-            return join(",", array(
-                "El señor de los anillos",
-                "Los límites de la Fundación",
-                "The Rails Way"));
-        }
-        else {
-            return "No hay productos de esta categoria";
-        }*/
+
+require_once "nusoap/lib/nusoap.php";
+
+$server = new soap_server();
+$server->configureWSDL("producto", "urn:producto");
+$server->register("getInsertarEmpleado", array(
+    "tipoRegistro" => "xsd:integer",
+    "codigoTipoIdentificacion" => "xsd:string",
+    "identificacion" => "xsd:integer",
+    "nombre1" => "xsd:string",
+    "nombre2" => "xsd:string",
+    "apellido1" => "xsd:string",
+    "apellido2" => "xsd:string"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarEmpleado", "rpc", "encoded", "Insertar empleados");
+
+/*$server->register("getInsertarPago", array(
+    "tipoRegistro" => "xsd:integer",
+    "tipoPago" => "xsd:string",
+    "fechaDesde" => "xsd:date",
+    "fecahHasta" => "xsd:date",
+    "numero" => "xsd:string",
+    "vrDeduccion" => "xsd:integer",
+    "vrNeto" => "xsd:integer",
+    "vrDevengado" => "xsd:integer",
+    "tipodocumento" => "xsd:string",
+    "numeroDocumento" => "xsd:string"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarPago", "rpc", "encoded", "Insertar pago");
+
+$server->register("getInsertarPagoDetalle", array(
+    "tipoRegistro" => "xsd:integer",
+    "concepto" => "xsd:integer",
+    "vrPago" => "xsd:integer",
+    "operacion" => "xsd:integer",
+    "horas" => "xsd:integer",
+    "porcentaje" => "xsd:integer",
+    "dias" => "xsd:integer",
+    "vrNetoDetalle" => "xsd:integer",
+    "numeroDetalleFk" => "xsd:integer"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarPagoDetalle", "rpc", "encoded", "Insertar pago detalle");*/
+
+
+
+ function getInsertarEmpleado($paramsEmpleado) {
+    $servername = "localhost";
+    $username = "root1";
+    $password = "123456";
+    $dbname = "bdardid";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-    function getInsertarPago($codigoTipoIdentificacion, $identificacion) {
-        return "Se inserto el empleado";
-        /*if ($categoria == "libros") {
-            return join(",", array(
-                "El señor de los anillos",
-                "Los límites de la Fundación",
-                "The Rails Way"));
+    if ($paramsEmpleado['tipoRegistro'] == "1") {
+        $insert = "INSERT INTO empleado (codigo_identificacion_tipo_fk, numero_identifacion) VALUES ('1', '123456789')";
+    if ($conn->query($insert) === TRUE) {
+        $respuesta= "inserto";
+    } else {
+        $respuesta= "Error: " . $conn->error . $conn->connect_error;
+    }
+    }
+
+       /* 
+        * , $paramsEmpleado[nombre1],$paramsEmpleado[nombre2], $paramsEmpleado[apellido1], $paramsEmpleado[apellido2], $paramsEmpleado[nombrecorto] 
+        *  nombre1, nombre2, apellido1, apellido2, nombrecorto
+       */
+ 
+   
+
+   // var_dump($respuesta);
+    return  $respuesta ;
+}
+
+/*function getInsertarPago($paramsPago) {
+    $db = new db($_SESSION["host"], $_SESSION["usuario"], $_SESSION["pass"]);
+    $db->conectar();
+    if ($paramsPago[tipoRegistroPago] == "02") {
+        $insert = "INSERT INTO pago (codigo_pago_tipo_fk, fecha_desde, fecha_hasta, numero, vr_deducciones, vr_neto, vr_devengado) VALUES ($paramsPago[tipoPago],$paramsPago[fechaDesde],$paramsPago[fecahHasta] ,$paramsPago[numero], $paramsPago[vrDeduccion], $paramsPago[vrNeto], $paramsPago[vrDevengado], $paramsPago[tipodocumento], $paramsPago[numeroDocumento] )";
+        $db->query($insert);
+        if (empty($db->error)) {
+            $respuesta["Ok"] = true;
+            $respuesta["Mensaje"] = "se ah guardado con exito el pago.";
+        } else {
+            $respuesta["Ok"] = false;
+            $respuesta["Mensaje"] = $db->error;
         }
-        else {
-            return "No hay productos de esta categoria";
-        }*/
-    }      
-    $server = new soap_server();
-    $server->configureWSDL("producto", "urn:producto");
-      
-    $server->register("getInsertarEmpleado",
-        array("codigoTipoIdentificacion" => "xsd:string", "identificacion" => "xsd:string"),
-        array("return" => "xsd:string"),
-        "urn:administracion",
-        "urn:administracion#getInsertarEmpleado",
-        "rpc",
-        "encoded",
-        "Insertar empleados");
-    if ( !isset( $HTTP_RAW_POST_DATA ) ) $HTTP_RAW_POST_DATA =file_get_contents( 'php://input' );      
-    $server->service($HTTP_RAW_POST_DATA);
+    }
+    return new soapval('return', $respuesta);
+}*/
+
+/*function getInsertarPagoDetalle($paramsPagoDetalle) {
+    $db = new db($_SESSION["host"], $_SESSION["usuario"], $_SESSION["pass"]);
+    $db->conectar();
+    if ($paramsPagoDetalle[tipoRegistroPagoDetalle] == "03") {
+        $insert = "INSERT INTO pago_detalle (concepto, vr_pago, operacion, horas, porcentaje, dias, vr_pago_neto, codigo_numero_fk) VALUES ( $paramsPagoDetalle[concepto], $paramsPagoDetalle[vrPago],$paramsPagoDetalle[operacion], $paramsPagoDetalle[horas], $paramsPagoDetalle[porcentaje], $paramsPagoDetalle[dias], $paramsPagoDetalle[vrNetoDetalle],$paramsPagoDetalle[numeroDetalleFk] )";
+        $db->query($insert);
+        if (empty($db->error)) {
+            $respuesta["Ok"] = true;
+            $respuesta["Mensaje"] = "se ah guardado con exito el pago detalle.";
+        } else {
+            $respuesta["Ok"] = false;
+            $respuesta["Mensaje"] = $db->error;
+        }
+    }
+    return new soapval('return', $respuesta);
+}*/
+
+
+if (!isset($HTTP_RAW_POST_DATA))
+    $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+$server->service($HTTP_RAW_POST_DATA);
 ?>
