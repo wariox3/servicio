e75e09a405ca280e61a4639761ce30c773837bfb
desarrollo <desarrollo@desarrollo-MS-7817>
2017-01-11 19:50:41 -0500

    Varios

--- cliente.php
+++ cliente.php
@@ -1,101 +1,37 @@
 <?php
-    require_once "nusoap/lib/nusoap.php";
-    $cliente = new nusoap_client("http://localhost/servicio/servidor.php");
 
-    if ($error) {
-        echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
-    }
-    
-    
-    //datos empleado__________________________________
-    $tipoRegistro = 1;
-    $codigoTipoIdentificacion = 1;
-    $identificacion = 70143086;
-    $nombre1 = "jonathan";
-    $nombre2 = "alexander";
-    $apellido1 = "moncada";
-    $apellido2 = "duque";
-    $nombrecorto = $nombre1 . $nombre2 . $apellido1 . $apellido2;
-    
-    $paramsEmpleado = array(
-    'tipoRegistro' => $tipoRegistro,
-    'codigoTipoIdentificacion' => $codigoTipoIdentificacion,
-    'identificacion' => $identificacion,
-    'nombre1' => $nombre1,
-    'nombre2' => $nombre2,
-    'apellido1' => $apellido1,
-    'apellido2' => $apellido2,
-    'nombrecorto' => $nombrecorto);
+require_once "nusoap/lib/nusoap.php";
+$cliente = new nusoap_client("http://localhost/servicio/servidor.php");
 
-    //datos pago________________________________________
-   /* $tipoRegistroPago = 02;
-    $tipoPago = 1;
-    $fechaDesde = 2016-01-04;
-    $fechaHasta = 2016-01-08;
-    $numero = 23456789;
-    $vrDeduccion = 98;
-    $vrNeto = 87;
-    $vrDevengado = 76;
-    $tipodocumento = 1;
-    $numeroDocumento = 1234567;
-    
-    $paramsPago = array(
-    'tipoRegistroPago' => $tipoRegistroPago,
-    'tipoPago' => $tipoPago,
-    'fechaDesde' => date_create($fechaDesde),
-    'fechaHasta' => date_create($fechaHasta),
-    'numero' => $numero,
-    'vrDeduccion' => $vrDeduccion,
-    'vrNeto' => $vrNeto,
-    'vrDevengado' => $vrDevengado,
-    'tipodocumento' => $tipodocumento,
-    'numeroDocumento' => $numeroDocumento);*/
+$error = $cliente->getError();
+if ($error) {
+    echo "<h2>Constructor error</h2><pre>" . $error . "</pre>";
+}      
 
-    //datos pagoDetalle_____________________________________
-    /*$tipoRegistroPagoDetalle = 03;
-    $concepto = " concepto de pago por prestacion de servicios";
-    $vrPago = 12;
-    $operacion = 10;
-    $horas = 5;
-    $porcentaje = 8;
-    $dias = 7;
-    $vrNetoDetalle = 78987;
-    $numeroDetalleFk = 88888855;
-    
-    $paramsPagoDetalle = array(
-    'tipoRegistroPagoDetalle' => $tipoRegistroPagoDetalle,
-    'concepto' => $concepto,
-    'vrPago' => $vrPago,
-    'operacion' => $operacion,
-    'horas' => $horas,
-    'porcentaje' => $porcentaje,
-    'dias' => $dias,
-    'vrNetoDetalle' => $vrNetoDetalle,
-    'numeroDetalleFk' => $numeroDetalleFk);*/
-     
-    $result = $cliente->call("getInsertarEmpleado", $paramsEmpleado );
-    
-    //$result = $cliente->call("getInsertarPago", $paramsPago);
+    $result = $cliente->call("getInsertarEmpleado", 
+            array(
+                "codigoIdentificacionTipo" => "CC", 
+                "identificacionNumero" => "70143086",
+                "nombre1" => "MARIO",
+                "nombre2" => "ANDRES",
+                "apellido1" => "ESTRADA",
+                "apellido2" => "ZULUAGA",
+                "nombreCorto" => "MARIO ANDRES ESTRADA ZULUAGA",));
 
-   // $result = $cliente->call("getInsertarPagoDetalle", $paramsPagoDetalle);
-
-    if ($cliente->fault) {
-        echo "<h2>Fault</h2><pre>";
-           print_r($respuesta);
-        echo "</pre>";
-    }
-    else {
-    print_r($respuesta);
-        $error = $cliente->getError(); 
-       $client->request;
-        var_dump($cliente); // prints "test"
-        if ($error) {
-            echo "<h2>Error</h2><pre>" . $error . "</pre>";
-        }
-        else {
-            echo "<h2>Cargar Datos</h2><pre>";
-            echo $result;
-            echo "</pre>";
-        }
-    }
+if ($cliente->fault) {
+  echo "<h2>Fault</h2><pre>";
+  print_r($result);
+  echo "</pre>";
+  }
+  else {
+  $error = $cliente->getError();
+  if ($error) {
+  echo "<h2>Error</h2><pre>" . $error . "</pre>";
+  }
+  else {
+  echo "<h2>Libros</h2><pre>";
+  echo $result;
+  echo "</pre>";
+  }
+  }
 ?>
--- servidor.php
+++ servidor.php
@@ -5,101 +5,68 @@ require_once "nusoap/lib/nusoap.php";
 $server = new soap_server();
 $server->configureWSDL("producto", "urn:producto");
 $server->register("getInsertarEmpleado", array(
-    "tipoRegistro" => "xsd:integer",
-    "codigoTipoIdentificacion" => "xsd:string",
-    "identificacion" => "xsd:integer",
+    "codigoIdentificacionTipo" => "xsd:string",
+    "identificacionNumero" => "xsd:string",
     "nombre1" => "xsd:string",
     "nombre2" => "xsd:string",
     "apellido1" => "xsd:string",
-    "apellido2" => "xsd:string"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarEmpleado", "rpc", "encoded", "Insertar empleados");
+    "apellido2" => "xsd:string",
+    "nombreCorto" => "xsd:string"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarEmpleado", "rpc", "encoded", "Insertar empleados");
 
-/*$server->register("getInsertarPago", array(
-    "tipoRegistro" => "xsd:integer",
-    "tipoPago" => "xsd:string",
-    "fechaDesde" => "xsd:date",
-    "fecahHasta" => "xsd:date",
-    "numero" => "xsd:string",
-    "vrDeduccion" => "xsd:integer",
-    "vrNeto" => "xsd:integer",
-    "vrDevengado" => "xsd:integer",
-    "tipodocumento" => "xsd:string",
-    "numeroDocumento" => "xsd:string"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarPago", "rpc", "encoded", "Insertar pago");
+$server->register("getInsertarPago", array(
+    "codigoEmpresa" => "xsd:integer",
+    "numero" => "xsd:integer",
+    "nombre1" => "xsd:string"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarPago", "rpc", "encoded", "Insertar pago");
 
-$server->register("getInsertarPagoDetalle", array(
-    "tipoRegistro" => "xsd:integer",
-    "concepto" => "xsd:integer",
-    "vrPago" => "xsd:integer",
-    "operacion" => "xsd:integer",
-    "horas" => "xsd:integer",
-    "porcentaje" => "xsd:integer",
-    "dias" => "xsd:integer",
-    "vrNetoDetalle" => "xsd:integer",
-    "numeroDetalleFk" => "xsd:integer"), array("return" => "xsd:string"), "urn:administracion", "urn:administracion#getInsertarPagoDetalle", "rpc", "encoded", "Insertar pago detalle");*/
-
-
-
- function getInsertarEmpleado($paramsEmpleado) {
-    $servername = "localhost";
-    $username = "root1";
-    $password = "123456";
-    $dbname = "bdardid";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+function getInsertarEmpleado($codigoIdentificacionTipo, $identificacionNumero, $nombre1, $nombre2, $apellido1, $apellido2, $nombreCorto) {
+    $respuesta = "01";
+    $servidor = new mysqli("localhost", "root", "70143086", "bdardid");
+    if ($servidor->connect_error) {
+        die("Connection failed: " . $servidor->connect_error);
     }
-    if ($paramsEmpleado['tipoRegistro'] == "1") {
-        $insert = "INSERT INTO empleado (codigo_identificacion_tipo_fk, numero_identifacion) VALUES ('1', '123456789')";
-    if ($conn->query($insert) === TRUE) {
-        $respuesta= "inserto";
-    } else {
-        $respuesta= "Error: " . $conn->error . $conn->connect_error;
+    $strSql = "SELECT codigo_empleado_pk FROM empleado WHERE codigo_identificacion_tipo_fk = '" . $codigoIdentificacionTipo . "' AND identificacion_numero = '" . $identificacionNumero . "'";
+    if ($sentencia = $servidor->prepare($strSql)) {
+        $sentencia->execute();
+        $sentencia->store_result();
+        if ($sentencia->num_rows <= 0) {
+            $strSql = "INSERT INTO empleado (codigo_identificacion_tipo_fk, identificacion_numero, nombre1, nombre2, apellido1, apellido2, nombre_corto) VALUES ('$codigoIdentificacionTipo', '$identificacionNumero', '$nombre1', '$nombre2', '$apellido1', '$apellido2', '$nombreCorto');";
+            if ($servidor->query($strSql) === TRUE) {
+                $respuesta = "01";
+            } else {
+                $respuesta = "02";
+            }
+        } else {
+            $respuesta = "01";
+        }
+        $sentencia->close();
     }
-    }
-
-       /* 
-        * , $paramsEmpleado[nombre1],$paramsEmpleado[nombre2], $paramsEmpleado[apellido1], $paramsEmpleado[apellido2], $paramsEmpleado[nombrecorto] 
-        *  nombre1, nombre2, apellido1, apellido2, nombrecorto
-       */
- 
-   
-
-   // var_dump($respuesta);
-    return  $respuesta ;
+    return $respuesta;
 }
 
-/*function getInsertarPago($paramsPago) {
-    $db = new db($_SESSION["host"], $_SESSION["usuario"], $_SESSION["pass"]);
-    $db->conectar();
-    if ($paramsPago[tipoRegistroPago] == "02") {
-        $insert = "INSERT INTO pago (codigo_pago_tipo_fk, fecha_desde, fecha_hasta, numero, vr_deducciones, vr_neto, vr_devengado) VALUES ($paramsPago[tipoPago],$paramsPago[fechaDesde],$paramsPago[fecahHasta] ,$paramsPago[numero], $paramsPago[vrDeduccion], $paramsPago[vrNeto], $paramsPago[vrDevengado], $paramsPago[tipodocumento], $paramsPago[numeroDocumento] )";
-        $db->query($insert);
-        if (empty($db->error)) {
-            $respuesta["Ok"] = true;
-            $respuesta["Mensaje"] = "se ah guardado con exito el pago.";
-        } else {
-            $respuesta["Ok"] = false;
-            $respuesta["Mensaje"] = $db->error;
-        }
+function getInsertarPago($codigoEmpresa, $numero) {
+    $respuesta = "01";
+    $servidor = new mysqli("localhost", "root", "70143086", "bdardid");
+    if ($servidor->connect_error) {
+        die("Connection failed: " . $servidor->connect_error);
     }
-    return new soapval('return', $respuesta);
-}*/
-
-/*function getInsertarPagoDetalle($paramsPagoDetalle) {
-    $db = new db($_SESSION["host"], $_SESSION["usuario"], $_SESSION["pass"]);
-    $db->conectar();
-    if ($paramsPagoDetalle[tipoRegistroPagoDetalle] == "03") {
-        $insert = "INSERT INTO pago_detalle (concepto, vr_pago, operacion, horas, porcentaje, dias, vr_pago_neto, codigo_numero_fk) VALUES ( $paramsPagoDetalle[concepto], $paramsPagoDetalle[vrPago],$paramsPagoDetalle[operacion], $paramsPagoDetalle[horas], $paramsPagoDetalle[porcentaje], $paramsPagoDetalle[dias], $paramsPagoDetalle[vrNetoDetalle],$paramsPagoDetalle[numeroDetalleFk] )";
-        $db->query($insert);
-        if (empty($db->error)) {
-            $respuesta["Ok"] = true;
-            $respuesta["Mensaje"] = "se ah guardado con exito el pago detalle.";
+    $strSql = "SELECT codigo_pago_pk FROM pago WHERE codigo_empresa_fk = " . $codigoEmpresa . " AND numero = '" . $numero . "'";
+    if ($sentencia = $servidor->prepare($strSql)) {
+        $sentencia->execute();
+        $sentencia->store_result();
+        if ($sentencia->num_rows <= 0) {
+            $strSql = "INSERT INTO pago (codigo_empresa_fk, numero) VALUES ($codigoEmpresa, $numero);";
+            if ($servidor->query($strSql) === TRUE) {
+                $respuesta = "01";
+            } else {
+                $respuesta = "02";
+            }
         } else {
-            $respuesta["Ok"] = false;
-            $respuesta["Mensaje"] = $db->error;
+            $respuesta = "01";
         }
+        $sentencia->close();
     }
-    return new soapval('return', $respuesta);
-}*/
+    return $respuesta;
+}
 
 
 if (!isset($HTTP_RAW_POST_DATA))
