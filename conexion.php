cb3a66133a574aa39196e928a7c02a85bc1afb1d
Juan Felipe Mesa Ocampo <felipemesa14@gmail.com>
2018-10-24 08:56:11 -0500

    feat(Se agrega campo mensaje de pago):
    Se agrega nombre mensaje de pago al momento de que consuman el servicio web

--- conexion.php
+++ conexion.php
@@ -1,7 +1,8 @@
 <?php
 
-function conectar() {
-    $servidor = new mysqli("localhost", "root", "70143086", "bdardid");
+function conectar()
+{
+    $servidor = new mysqli("localhost", "root", "70143086", "bdardi");
     if ($servidor->connect_error) {
         die("Connection failed: " . $servidor->connect_error);
     }
